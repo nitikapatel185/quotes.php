3095efc16a03de9b642ed93c029ab1955b869a56
Nitika Patel <68534716+nitikapatel185@users.noreply.github.com>
2021-06-05 18:33:06 +0530

    Add files via upload

---
+++ quote.php
@@ -0,0 +1,16 @@
+<?php
+echo "<title> php exmple</title>";
+echo " double and single quotes";
+echo "<br>==================</br>";
+
+echo 'single quotes<br/>
+  <p style ="font-family:$sans-serif;color:red">this is a single quote paragraph</p>";
+ <br/>================<br/>"';
+
+echo "double quotes<br/>;
+    <p style=\"font-family:$sans-serif;color:green\">
+    This is double quotes paragraph
+    </p>
+    ";
+ echo"<br/>===========<br/>";
+?>
